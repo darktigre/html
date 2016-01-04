d54a75ca0fb24ff958bd8c29e8d5ab8db787c653
darktigre <tigre_joaquin@hotmail.com>
2016-01-03 19:12:50 -0600

    Signed-off-by: darktigre <tigre_joaquin@hotmail.com>

---
+++ app/app.js
@@ -0,0 +1,60 @@
+/*(function myblog() {
+var app = function(){
+	function getSections(id){
+		return document.getElementsByTagName('id');
+	}
+	var publicAPI = {
+		getSections:getSections
+	}
+	return publicAPI;
+}();
+log(app.getSections('section'));
+})();
+*/
+
+// queryAll
+
+/*(function(){ //forma nativa
+	var app = {
+		myDOMapi: domApinFunc(),
+		addSections: addSectionsFunc,
+		updateArticle: updateArticle
+	}
+})();
+
+
+function domApiFunc(){// foma jequery
+function getSections(id){
+	return document.queryAllSelectorAll(id);
+}
+var publicAPI ={
+	getSections: getSections
+}
+return publicAPI;
+};
+
+$("#main-conteiner").animate({}, function(){ // codigo muy sucio
+	$(this).animate({},function(){
+		$(this).animate({},function())
+	})
+});
+})();
+
+
+app.init();
+function init(){
+
+}
+function addSectiosFunc(){
+	this.main-conteiner = this.myDOMapi.getSectionsContei
+}*/
+
+
+
+
+
+
+
+
+
+
